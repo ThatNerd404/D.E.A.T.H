65c43c7755d23d444aece386929c3887cd29397f
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-10-06 20:53:34 -0500

    Organizing everything

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
+# Main file for connecting function to the gui
 import PyQt5
 from PyQt5 import QtWidgets 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtGui import QPalette, QColor
 import sys
-from QT_test import Ui_MainWindow
+from Gui.QT_test import Ui_MainWindow
+
 class Mainwindow(QMainWindow,Ui_MainWindow):
     def __init__(self):
         super(Mainwindow,self).__init__()
