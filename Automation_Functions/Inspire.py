a1ed9a032b9bf5ea27c9acfb8cd31c09b2d13852
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-10-08 21:15:51 -0500

    finished class

--- Automation_Functions/Inspire.py
+++ Automation_Functions/Inspire.py
@@ -2,11 +2,22 @@
 
 import requests
 
-responce  = requests.get("https://motivational-quote-api.herokuapp.com/quotes/random")
-res = responce.json()
-quote = res['quote']
-author = res['person']
+class Inspire:
+    def __init__(self):
+        pass
 
-print(f"quote: {quote} author: {author}")
-#returns a dict need ot learn how to grab data from dict to print or store
-#what i want in a variable 
+    def Inspiration(self):
+        responce  = requests.get("https://motivational-quote-api.herokuapp.com/quotes/random")
+        res_dict = responce.json() #? returns dictonary
+        quote = res_dict['quote'] 
+        author = res_dict['person'] #? stores the value as a string
+
+        return quote , author #?returns tuple
+
+#? How to use |
+#?           \_/
+if __name__ == "__main__":
+    inspire = Inspire()
+    quote , author = inspire.Inspiration() #?turns tuple into string
+    print(f"Quote: {quote}")
+    print(f"Author: {author}")
