105ddd24c3684551276cb2ad8f26527e2a650d7a
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-10-08 21:02:40 -0500

    putting base functionality to soon turn into class

--- Automation_Functions/Inspire.py
+++ Automation_Functions/Inspire.py
@@ -1,13 +1,12 @@
 # Inspire.py - module to grab inspiration quotes from an api  
-import bs4
-import requests
-import json 
-responce  = requests.get("https://motivational-quote-api.herokuapp.com/quotes/random")
-res = responce.text
-'''weathersoup = bs4.BeautifulSoup(res.text, features= "lxml")
-temp_element = weathersoup.select('div p ')
-temp = str(temp_element[4].get_text())'''
 
-print(res)
+import requests
+
+responce  = requests.get("https://motivational-quote-api.herokuapp.com/quotes/random")
+res = responce.json()
+quote = res['quote']
+author = res['person']
+
+print(f"quote: {quote} author: {author}")
 #returns a dict need ot learn how to grab data from dict to print or store
 #what i want in a variable 
