c6379b4475a139e41bae59693d8004c4bea8084c
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-10-12 17:49:55 -0500

    basic system info

--- Automation_Functions/Stats.py
+++ Automation_Functions/Stats.py
@@ -1,13 +1,22 @@
 # Stats.py - Shows my system stats like ram, cpu , etc.
 # aka how fast is my computer today
-
+import platform
 
 
 
 class Stats:
     def __init__(self):
         pass
-
+    def check_stats(self):
+        my_system = platform.uname()
+ 
+        print(f"System: {my_system.system}")
+        print(f"Node Name: {my_system.node}")
+        print(f"Release: {my_system.release}")
+        print(f"Version: {my_system.version}")
+        print(f"Machine: {my_system.machine}")
+        print(f"Processor: {my_system.processor}")
 
 if __name__ == '__main__':
-    pass
+    S = Stats()
+    S.check_stats()
