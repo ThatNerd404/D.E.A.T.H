b2a62879b0614b194e14df9fd2fc1a2f35d6454b
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-10-12 12:10:01 -0500

    creating stats module

---
+++ Automation_Functions/Stats.py
@@ -0,0 +1,13 @@
+# Stats.py - Shows my system stats like ram, cpu , etc.
+# aka how fast is my computer today
+
+
+
+
+class Stats:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
