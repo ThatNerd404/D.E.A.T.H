71e945e8e5f8d03f9bb0d18d7471e167c40f9dd9
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-10-24 20:24:32 -0500

    testing variable swapping

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget,QPushButton
 from PyQt5.QtGui import QPalette, QColor
 import sys
 from Gui.QT_test import Ui_MainWindow
-
+from Automation_Functions.Inspire import Inspire
 class Mainwindow(QMainWindow,Ui_MainWindow):
     def __init__(self):
         super(Mainwindow,self).__init__()
@@ -14,9 +14,9 @@ class Mainwindow(QMainWindow,Ui_MainWindow):
         self.ui.setupUi(self)
         flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setWindowFlags(flags)
-        self.ui.Thatbutton.setText("Exit")
         self.ui.Thatbutton.clicked.connect(lambda: self.close()) # dont add () if you dont add stuff in it 
-        
+        #inspire = Quote, Author = Inspire.Inspiration(self) <-- How I will set variable data to labels aka dont forget self
+        #self.ui.Thatbutton.setText(Quote)
     def clicked(self):
         print("clicked")
     
