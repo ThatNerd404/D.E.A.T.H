fe5d2da033657bd5c3de794637ea2e7fe83eb561
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-10-26 21:32:28 -0500

    fixed full-screen problem

--- Main.py
+++ Main.py
@@ -7,20 +7,35 @@ from PyQt5.QtGui import QPalette, QColor
 import sys
 from Gui.Main_Gui import Ui_MainWindow
 from Automation_Functions.Inspire import Inspire
+
+WINDOW_SIZE = 0
+
 class Mainwindow(QMainWindow,Ui_MainWindow):
     def __init__(self):
         super(Mainwindow,self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        #self.ui.Main_Content.setFixedSize(self.sizeHint())
+
         flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setWindowFlags(flags)
+        self.ui.Home_Button.clicked.connect(lambda: self.ui.Pages.setCurrentWidget(self.ui.Home_Page))
+        self.ui.Time_Button.clicked.connect(lambda: self.ui.Pages.setCurrentWidget(self.ui.Time_Reminders_Page))
         self.ui.Close_Button.clicked.connect(lambda: self.close()) # dont add () if you dont add stuff in it 
+        self.ui.Maximize_Button.clicked.connect(lambda: self.restore_or_maximized())
         self.ui.Minimize_Button.clicked.connect(lambda: self.showMinimized())
         #inspire = Quote, Author = Inspire.Inspiration(self) #<-- How I will set variable data to labels aka dont forget self
         #self.ui.Thatbutton.setText(Quote)
         
-    def clicked(self):
-        print("clicked")
+    def restore_or_maximized(self):
+        global WINDOW_SIZE
+        win_status = WINDOW_SIZE
+        if win_status == 0:
+            WINDOW_SIZE = 1
+            self.showMaximized()
+        else:
+            WINDOW_SIZE = 0 
+            self.showNormal()
     
 
 def app():
