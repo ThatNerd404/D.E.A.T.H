f068f6959ed8caa9dfbf4a7d9024a4006a526174
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-11-01 20:44:59 -0500

    doing save files

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 
 import PyQt5
 from PyQt5 import QtWidgets ,QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget,QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget,QPushButton , QFileDialog
 from PyQt5.QtGui import QPalette, QColor
 import sys
 from Gui.Main_Gui import Ui_MainWindow
@@ -14,18 +14,38 @@ class Mainwindow(QMainWindow,Ui_MainWindow):
     def __init__(self):
         super(Mainwindow,self).__init__()
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        self.ui.setupUi(self) 
+        #? starting widget
         self.ui.Pages.setCurrentWidget(self.ui.Home_Page)
+         #? setting start widget page
         flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
-        self.setWindowFlags(flags)
+        self.setWindowFlags(flags)#? getting rid of frame
+
+        workouts_data = self.loadInput("Workouts_Save_File.txt")
+        notes_data = self.loadInput("Notes_Save_File.txt") #? Grab data from save files
+        self.ui.Workouts_text_edit.setText(workouts_data)
+        self.ui.Notes_text_edit.setText(notes_data)
         #? Setting buttons functions 
+
         self.ui.Home_Button.clicked.connect(lambda: self.ui.Pages.setCurrentWidget(self.ui.Home_Page))
         self.ui.Time_Button.clicked.connect(lambda: self.ui.Pages.setCurrentWidget(self.ui.Time_Reminders_Page))
         self.ui.Close_Button.clicked.connect(lambda: self.close()) # dont add () if you dont add stuff in it 
         self.ui.Maximize_Button.clicked.connect(lambda: self.restore_or_maximized())
         self.ui.Minimize_Button.clicked.connect(lambda: self.showMinimized())
+        self.ui.Workout_Save_Button.clicked.connect(lambda: self.saveInput(self.ui.Workouts_text_edit,"Workouts_Save_File.txt"))
+        self.ui.Notes_Save_Button.clicked.connect(lambda: self.saveInput(self.ui.Notes_text_edit,"Notes_Save_File.txt"))
+
         #inspire = Quote, Author = Inspire.Inspiration(self) #<-- How I will set variable data to labels aka dont forget self
         #self.ui.Thatbutton.setText(Quote)
+    def loadInput(self,file):
+        with open(file,"r") as f:
+            data = f.read()
+        return data 
+        
+    def saveInput(self,widget,file): 
+        contents = widget.toPlainText() 
+        with open(file,"w") as f:
+                f.write(contents)
         
     def restore_or_maximized(self):
         global WINDOW_SIZE
