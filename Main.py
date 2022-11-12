eaf8f850cafe96682d275f92695ea7779d83f668
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-11-11 20:39:05 -0600

    fixing modules

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from Automation_Functions.Chrono import Chrono
+from Automation_Functions.Sky import Sky
 from Gui.Main_Gui import Ui_MainWindow
 
 # The Starting window size
@@ -20,11 +21,14 @@ class Mainwindow(QMainWindow,Ui_MainWindow):
         
         #? Grabbing needed data from module to display on gui
         Cron = Chrono()
-        S = Sky()
+        
+        #* Using seperate functions to grab Datetime so I can make a thread to constantly check time
         Date_Text = Cron.Get_Date()
         Time_Text = Cron.Get_Time()
         Xmas_Countdown_Text = Cron.Days_Till_Xmas()
 
+        S = Sky()
+        Weather_Text, Temperature_Text, Feels_Like_Text = S.Fetch_Weather_Data()
 
         #? getting rid of frame
         flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint)
@@ -41,10 +45,6 @@ class Mainwindow(QMainWindow,Ui_MainWindow):
         self.ui.Date_Label.setText(Date_Text)
         self.ui.Time_Label.setText(Time_Text)
         self.ui.Xmas_Countdown_Label.setText(f"{Xmas_Countdown_Text} Days 'Till Christmas!")
-        
-        self.ui.Weather_Label.setText(f"Weather: {Weather_Text}")
-        self.ui.Temperature_Label.setText(f"Temperature: {Temperature_Text}")
-        self.ui.Feels_Like_Label.setText(f"Feels like: {Feels_Like_Text}")
 
         #? Setting buttons functions 
         self.ui.Home_Button.clicked.connect(lambda: self.ui.Pages.setCurrentWidget(self.ui.Home_Page))
