9fb126f75ec77da4157be181bc039532ef6e37bb
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2022-11-12 23:30:15 -0600

    setting labels for data

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@
 import sys
 
 from PyQt5 import QtCore
+from PyQt5.QtGui import QPixmap, QIcon 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from Automation_Functions.Chrono import Chrono
@@ -46,6 +47,16 @@ class Mainwindow(QMainWindow,Ui_MainWindow):
         self.ui.Time_Label.setText(Time_Text)
         self.ui.Xmas_Countdown_Label.setText(f"{Xmas_Countdown_Text} Days 'Till Christmas!")
 
+        self.ui.Weather_Label.setText(f"Weather: {Weather_Text}")
+        self.ui.Temperature_Label.setText(f"Temperature: {Temperature_Text}")
+        self.ui.Feels_Like_Label.setText(f"Feels Like: {Feels_Like_Text}")
+
+        if Weather_Text == "Clear":
+            self.ui.Weather_Status_Pic.setPixmap(QPixmap('Gui/icons8-clouds-64.png'))
+
+        elif Weather_Text == "Clouds":
+            self.ui.Weather_Status_Pic.setPixmap(QPixmap('Gui/icons8-sun-64.png'))
+        
         #? Setting buttons functions 
         self.ui.Home_Button.clicked.connect(lambda: self.ui.Pages.setCurrentWidget(self.ui.Home_Page))
         self.ui.Time_Button.clicked.connect(lambda: self.ui.Pages.setCurrentWidget(self.ui.Time_Reminders_Page))
@@ -56,10 +67,9 @@ class Mainwindow(QMainWindow,Ui_MainWindow):
         self.ui.Workout_Save_Button.clicked.connect(lambda: self.Save_Gui_Input(self.ui.Workouts_text_edit,"Save_Folder/Workouts_Save_File.txt"))
         self.ui.Notes_Save_Button.clicked.connect(lambda: self.Save_Gui_Input(self.ui.Notes_text_edit,"Save_Folder/Notes_Save_File.txt"))
 
-    #TODO: get rid of this function because you just use a long if else statement
-    def Set_Weather_Pic(self):
-        #QlabelWidget.setPixmap(QPixmap('your.jpg'))
-        pass
+        
+        
+        
 
     #? Save all text from file to save_data variable
     def Load_File_Input(self,file):
@@ -87,7 +97,9 @@ class Mainwindow(QMainWindow,Ui_MainWindow):
 def app():
     app = QApplication(sys.argv)
     win = Mainwindow()
-    win.showMaximized()
+    win.setWindowTitle("D.E.A.T.H - Developer Environment Automation & Task Handler")
+    win.setWindowIcon(QIcon('Gui/icons8-headstone-100.png'))
+    win.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
